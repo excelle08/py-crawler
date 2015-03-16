e95ed5f11807af760f6716a282706aa070f2b527
Excelle Su <2014060104018@std.uestc.edu.cn>
2015-03-16 22:20:49 +0800

    Update 0.1.1
    A formal release.
    Add necessary comments and fix some bugs

--- crawl.py
+++ crawl.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Excelle'
 
+'''
+    Net Crawler v0.1.1
+    Usage: python crawl.py <url>
+    This program will crawl your assigned website and download all file on it.
+    To have it run on the background, please use command:
+    $ nohup python crawl.py <url> > log.txt &
+'''
+
 import sys, re, os
 import urllib2
 import time
@@ -16,11 +24,35 @@ _LOGFILE = 'cralwer.log'
 
 url_list = Queue()
 base_url = ''
-base_dir = ''
+# Set if display verbose log.
 debug_mode = True
-p = Pool(processes=32)
+# Set whether to allow the program to open another main process to crawl external website
+# NOTE: This will probably consume a large amount of system resource.
+#       As a consequence, considerable lagging or even crash may happen.
+recursive_crawl = False
+# Size of the process pool
+pool_size = 32
+
+p = Pool(processes=pool_size)
+
+'''
+    In common URL parameters in HTML tags are like these:
+    1. A complete URL with a dedicated file (e.g. http://www.example.com/xxx/abc.php?par1=x&par2=y)
+        This form is usually implemented when we need to point to or source from an external link,
+         but there is possibility when it's linked to the original site.
+    2. A complete URL without a file (e.g. http://www.example.com/xxx/)
+        This is an implicit reference of default file such as index.html/index.htm/index.php/index.aspx
+    3. With path only.
+        NOTE: In this
+'''
+
 
 def addFileName(url):
+    '''
+    Append filename 'index.html' when a given url has no dedicated filename
+    :param url: Given URL
+    :return: Processed URL
+    '''
     list = re.split('/', url)
     if not list[-1]:
         return url + 'index.html'
@@ -28,9 +60,34 @@ def addFileName(url):
 
 
 def getDirectory(url):
+    '''
+    Get the directory of a given path (Trim the file name)
+    :param url: Given URL
+    :return: Processed URL
+    '''
     list = re.split('/', url)
     list.pop(-1)
     return string.join(list, '/')
+
+
+def procSrc(src):
+    '''
+    Process given URL parameters in HTML tags
+    :param src: Given URL
+    :return: Nothing
+    '''
+    p = urlparse(src)
+    if (p.netloc == base_url) or (not p.netloc):
+        if not os.path.exists('./' + p.netloc + addFileName(p.path)):
+            url_list.put(src)
+            log('Put %s into queue.' % src)
+    else:
+        if recursive_crawl:
+            log('Opening up a new process to crawl ' + src)
+            os.system('nohup python ' + sys.argv[0] + ' http://' + p.netloc + ' > log'
+                      + str(time.time()) + '.txt' + ' &')
+        else:
+            log('Ignored external link ' + src)
 
 
 def log(info):
@@ -60,13 +117,9 @@ class MyHTMLParser(HTMLParser):
         for key, value in attrs:
             attr[key] = value
         if tag == 'script' and attr['src']:
-            if not os.path.exists('./' + base_dir + addFileName(attr['src'])):
-                url_list.put(attr['src'])
-                log('Put %s into queue' % attr['src'])
+            procSrc(attr['src'])
         elif tag == 'a' and attr['href']:
-            if not os.path.exists('./' + base_dir + addFileName(attr['href'])):
-                url_list.put(attr['href'])
-                log('Put %s into queue' % attr['href'])
+            procSrc(attr['href'])
 
     def handle_startendtag(self, tag, attrs):
         attr = dict()
@@ -75,14 +128,10 @@ class MyHTMLParser(HTMLParser):
         for key, value in attrs:
             attr[key] = value
         if tag == 'img' and attr['src']:
-            if not os.path.exists('./' + base_dir + addFileName(attr['src'])):
-                url_list.put(attr['src'])
-                log('Put %s into queue' % attr['src'])
+            procSrc(attr['src'])
 
         elif tag == 'link' and attr['rel']:
-            if not os.path.exists('./' + base_dir + addFileName(attr['rel'])):
-                url_list.put(attr['rel'])
-                log('Put %s into queue' % attr['rel'])
+            procSrc(attr['rel'])
 
 '''
     UA-safari:
@@ -105,25 +154,26 @@ def worker_once():
         if os.path.exists('./' + base_dir + addFileName(parts.path)):
             return
 
-
+        # Create HTTP request object
         reqobj = urllib2.Request(url)
         reqobj.add_header('Host', parts.netloc)
         reqobj.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
                                         ' AppleWebKit/600.3.18 (KHTML, like Gecko) Version/7.1.3 Safari/537.85.12')
-        reqobj.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+        reqobj.add_header('Accept', 'ftext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
         reqobj.add_header('Accept-Language', 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3')
         log('Crawling: %s' % url)
         url_opener = urllib2.urlopen(reqobj)
         data = url_opener.read()
+        # Parse HTML data
         htmlparse = MyHTMLParser()
         htmlparse.feed(data)
 
         try:
-            os.makedirs(getDirectory('./' + base_dir + parts.path))
+            os.makedirs(getDirectory('./' + os.path.join(base_dir + parts.path)))
         except OSError:
             pass
-
-        with open('./' + base_dir + addFileName(parts.path), 'w+') as fp:
+        # Write data
+        with open('./' + os.path.join(base_dir + addFileName(parts.path)), 'w+') as fp:
             fp.write(data)
         log('Write file into: %s' % parts.path)
     except Exception, ex:
@@ -141,14 +191,14 @@ if __name__ == '__main__':
             log('Launched parent process: %s' % os.getpid())
             log('Doing the first iteration...')
             worker()
-            for i in range(32):
+            for i in range(pool_size):
                 p.apply_async(worker, args=())
             print('Doing all processes...')
             p.close()
             p.join()
             log('All process done.')
-        except BaseException, ex:
-            exception(ex)
+        except BaseException, e:
+            exception(e)
         finally:
             if not url_list.empty():
                 with open('status.last', 'w') as f:
@@ -167,6 +217,10 @@ if __name__ == '__main__':
             with open('./baseurl.last', 'r') as fp:
                 base_url = fp.readline()
             worker()
-            for i in range(32):
+            for i in range(pool_size):
                 p.apply_async(worker, args=())
+                print('Doing all processes...')
+                p.close()
+                p.join()
+                print('All processes done.')
         print('An argument about the website URL required.')
